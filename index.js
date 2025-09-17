624cdc42249d2cd62000f50e8be97b20922c041c
RageMonke <86595651+RageMonke@users.noreply.github.com>
2025-09-17 14:56:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildScheduledEvents,
+  ],
+});
+
+client.commands = new Collection();
+client.cooldowns = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.`);
+        }
+    }
+
+    // Add this code to load nested commands
+    const nestedFolders = fs.readdirSync(commandsPath).filter(f => !f.endsWith('.js'));
+    for (const nestedFolder of nestedFolders) {
+        const nestedPath = path.join(commandsPath, nestedFolder);
+        const nestedFiles = fs.readdirSync(nestedPath).filter(file => file.endsWith('.js'));
+
+        for (const file of nestedFiles) {
+            const filePath = path.join(nestedPath, file);
+            const command = require(filePath);
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.`);
+            }
+        }
+    }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(process.env.DISCORD_TOKEN);
+
